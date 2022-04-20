bd706962f0c9a49bdfd451e6d1ec632c46fee3e6
Jin Mok <jmok@veracode.com>
2022-04-20 16:13:29 -0700

    Merge remote-tracking branch 'origin/main' into develop

---
+++ app/src/main/java/com/mokkang/mokkang/MokkangApplication.java
@@ -0,0 +1,13 @@
+package com.mokkang.mokkang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokkangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokkangApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/mokkang/mokkang/MokkangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mokkang.mokkang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MokkangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
